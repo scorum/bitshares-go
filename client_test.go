f89fc284c8f3f47d416e27f5564f5333734b22cc
Vadim Pisaruk <pisarukvadim@gmail.com>
2018-06-05 15:52:26 +0300

    remove unused test

--- client_test.go
+++ client_test.go
@@ -1,13 +1,10 @@
 package openledger
 
 import (
-	"encoding/json"
-	"github.com/scorum/openledger-go/sign"
 	"github.com/scorum/openledger-go/types"
 	"github.com/stretchr/testify/require"
 	"log"
 	"testing"
-	"time"
 )
 
 const (
@@ -42,7 +39,7 @@ func TestClient_Transfer(t *testing.T) {
 	assets, err := client.Database.LookupAssetSymbols("TEST")
 	require.Nil(t, err)
 
-	scorum1 := "5JiTY3m9u1iPfoKsZdn18pnf26XvX2WnXFJckSiSaiUniNVzxLn"
+	cali4889IDActiveKey := "5JiTY3m9u1iPfoKsZdn18pnf26XvX2WnXFJckSiSaiUniNVzxLn"
 	from := cali4889ID
 	to := cali4890ID
 	amount := types.AssetAmount{
@@ -54,7 +51,7 @@ func TestClient_Transfer(t *testing.T) {
 		Amount:  0,
 	}
 
-	require.NoError(t, client.Transfer(scorum1, from, to, amount, fee))
+	require.NoError(t, client.Transfer(cali4889IDActiveKey, from, to, amount, fee))
 }
 
 /*
@@ -78,36 +75,3 @@ export const limit_order_cancel = new Serializer("limit_order_cancel", {
 
 // python tests
 // https://github.com/bitshares/python-bitshares/blob/9250544ca8eadf66de31c7f38fc37294c11f9548/tests/test_transactions.py
-
-func TestSign(t *testing.T) {
-	t.Skip()
-	//test_message := "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c810b9a4cb8255c93b155b0100b4280000000000000081bc3b95b20140420f000000000000000000"
-
-	op := types.TransferOperation{}
-	op.To = types.MustParseObjectID("1.2.22805")
-	op.From = types.MustParseObjectID("1.2.974337")
-	op.Fee = types.AssetAmount{
-		Amount:  10420,
-		AssetID: types.MustParseObjectID("1.3.0"),
-	}
-	op.Amount = types.AssetAmount{
-		Amount:  1000000,
-		AssetID: types.MustParseObjectID("1.3.0"),
-	}
-	op.Extensions = []json.RawMessage{}
-
-	tr := types.Transaction{}
-	tr.RefBlockPrefix = 1434635172
-	tr.RefBlockNum = 47376
-	ti := time.Unix(1528118217, 0)
-	tr.Expiration = types.Time{&ti}
-	tr.Operations = append(tr.Operations, &op)
-
-	st := sign.NewSignedTransaction(&tr)
-	_, err := st.Digest("4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	//TODO: Actually here we need to copare m
-}
